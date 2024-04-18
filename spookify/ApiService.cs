f9d45e02c3e3b915c86f249f148002f944ea7021
Denis Lucian Vararean <81750435+dezamagire@users.noreply.github.com>
2024-04-18 13:30:44 +0300

    attempt at connecting to backend

---
+++ spookify/ApiService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.IO;
+
+public class ApiService {
+    private readonly HttpClient _client;
+    private readonly string _destinationFolderPath;
+
+    public ApiService(string baseUrl, string destinationFolder) {
+        _destinationFolderPath = destinationFolder;
+        _client = new HttpClient {
+            BaseAddress = new Uri(baseUrl)
+        };
+        Console.WriteLine("Base address: " + baseUrl);
+    }
+
+    public async Task<bool> AddSongAsync(Song song) {
+        var response = await _client.PostAsJsonAsync("Songs", song);
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> UpdateSongAsync(Song song) {
+        var response = await _client.PutAsJsonAsync("Songs", song);
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> DeleteSongAsync(int songId) {
+        var response = await _client.DeleteAsync($"Songs/{songId}");
+        return response.IsSuccessStatusCode;
+    }
+
+    public async Task<Song> GetSongAsync(int songId) {
+        return await _client.GetFromJsonAsync<Song>($"Songs/{songId}");
+    }
+
+    public async Task<List<Song>> GetAllSongsAsync() {
+        return await _client.GetFromJsonAsync<List<Song>>("Songs");
+    }
+
+    public async Task<bool> DownloadSongFileAsync(Song song) {
+        try {
+            // Make the HTTP request to get the song file as a stream
+            var response = await _client.GetAsync($"Songs/{song.Id}/file", HttpCompletionOption.ResponseHeadersRead);
+            response.EnsureSuccessStatusCode();
+
+            Directory.CreateDirectory(_destinationFolderPath);
+
+            // Construct the local file path
+            var localFilePath = Path.Combine(_destinationFolderPath, song.Filename);  // Assumes file is in mp3 format
+
+            // Open a stream for the file to be written to
+            using (var fileStream = new FileStream(localFilePath, FileMode.Create, FileAccess.Write, FileShare.None)) {
+                // Copy the content from the response stream to the local file stream
+                await response.Content.CopyToAsync(fileStream);
+            }
+
+            return true;
+        } catch (Exception ex) {
+            // Log the error or handle it accordingly
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            return false;
+        }
+
+    }
+}
+
+public class Song {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Album { get; set; }
+    public string Artist { get; set; }
+    public int Streams { get; set; }
+    public string Filename { get; set; }
+
+}
--- spookify/MainWindow.xaml.cs
+++ spookify/MainWindow.xaml.cs
@@ -24,24 +24,28 @@ namespace spookify
 
         public MainWindow()
         {
+            
+            //ConfigurationHelper.InitializeConfiguration();
+            //string apiBaseUrl = ConfigurationHelper.GetApiBaseUrl();
+            //string destinationFolder = ConfigurationHelper.GetDestinationFolderPath();
+
+            
 
             InitializeComponent();
-
-            // binding for Songs
-            allSongs.Add(new Song(0, 4211, "Alison", "Nowhere", "Slowdive", "3:51", "C:/Users/alexa/Music/Slowdive/Nowhere/Alison.mp3"));
-            allSongs.Add(new Song(1, 4212, "Catch the Breeze", "Just for a Day", "Slowdive", "4:19", "C:/Users/alexa/Music/Slowdive/Just for a Day/Catch the Breeze.mp3"));
-            allSongs.Add(new Song(2, 4213, "A Strange Day", "Pornography", "The Cure", "5:04", "C:/Users/alexa/Music/The Cure/Pornography/A Strange Day.mp3"));
-            allSongs.Add(new Song(3, 4214, "Lullaby", "Disintegration", "The Cure", "4:10", "C:/Users/alexa/Music/The Cure/Disintegration/Lullaby.mp3"));
-            allSongs.Add(new Song(4, 4215, "When You Sleep", "Loveless", "My Bloody Valentine", "4:11", "C:/Users/alexa/Music/My Bloody Valentine/Loveless/When You Sleep.mp3"));
-            allSongs.Add(new Song(5, 4216, "Only Shallow", "Loveless", "My Bloody Valentine", "4:18", "C:/Users/alexa/Music/My Bloody Valentine/Loveless/Only Shallow.mp3"));
-            allSongs.Add(new Song(6, 4217, "Pictures of You", "Disintegration", "The Cure", "7:24", "C:/Users/alexa/Music/The Cure/Disintegration/Pictures of You.mp3"));
-            allSongs.Add(new Song(7, 4218, "Blue Light", "Souvlaki", "Slowdive", "5:20", "C:/Users/alexa/Music/Slowdive/Souvlaki/Blue Light.mp3"));
-            allSongs.Add(new Song(8, 4219, "Avalyn", "Just for a Day", "Slowdive", "5:05", "C:/Users/alexa/Music/Slowdive/Just for a Day/Avalyn.mp3"));
-            allSongs.Add(new Song(9, 4220, "Fascination Street", "Disintegration", "The Cure", "5:16", "C:/Users/alexa/Music/The Cure/Disintegration/Fascination Street.mp3"));
-            allSongs.Add(new Song(10,4221, "All I need", "In Rainbows", "Radiohead", "3:48", "C:/Users/alexa/Music/Radiohead/In Rainbows/All I need.mp3"));
             
-            // set songs to allSongs deepcopy for now
+            ApiService apiService = new ApiService(
+                "http://localhost:5006/",
+                "C:\\Users\\vdeniss\\Documents\\GitHub\\frontend\\temp");
 
+
+            // get song with id 1
+
+            Song song = apiService.GetSongAsync(1).Result;
+            
+            MessageBox.Show(song.Title+", "+song.Artist);
+
+            
+            
             songs = new ObservableCollection<Song>(allSongs);
             
             DataContext = songs;
@@ -50,30 +54,18 @@ namespace spookify
 
         private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
-        }
-
-        private void AutoSuggestBox_TextChanged(Wpf.Ui.Controls.AutoSuggestBox sender, Wpf.Ui.Controls.AutoSuggestBoxTextChangedEventArgs args)
-        {
-            string filter = sender.Text.ToLower();
-
-            songs.Clear();
-
-            foreach (Song song in allSongs)
-            {
-                if (song.Title.ToLower().Contains(filter) || song.Album.ToLower().Contains(filter) || song.Artist.ToLower().Contains(filter))
-                {
-                    songs.Add(song);
-                }
-            }
         }
 
         // select song on left click 
-        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void rowSelect(object sender, MouseButtonEventArgs e)
         {
             DataGridRow row = sender as DataGridRow;
             Song song = (Song)row.Item;
+            // select song
+            songsDataGrid.SelectedItem = song;
 
+            setCurrentSong(song);
+            
         }
 
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
@@ -104,6 +96,41 @@ namespace spookify
         private void ClickDelete(object sender, RoutedEventArgs e)
         {
             // delete selected song
+        }
+
+        private void DataGrid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var dep = (DependencyObject)e.OriginalSource;
+
+            // Find the clicked row
+            while (dep != null && !(dep is DataGridRow))
+            {
+                dep = VisualTreeHelper.GetParent(dep);
+            }
+
+            if (dep is DataGridRow row)
+            {
+                // Select the clicked row
+                if (!row.IsSelected)
+                {
+                    row.IsSelected = true;
+                }
+                
+                Song song = (Song)row.Item;
+                songsDataGrid.SelectedItem = song;
+
+                setCurrentSong(song);
+            }
+        }
+
+        private void setCurrentSong(Song song)
+        {
+            currentTitle.Text = song.Title;
+            currentArtist.Text = song.Artist;
+            currentAlbum.Text = song.Album;
+            currentId.Text = song.Id.ToString();
+            currentFilename.Text = song.Filename;
+            currentStreams.Text = song.Streams.ToString();
         }
     }
 }
